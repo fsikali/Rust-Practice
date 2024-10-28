638ba5f003d78e826292f7e8ecccab2971bc3aee
fsikali <flemmingsikali499@gmail.com>
2024-10-28 18:12:57 +0300

    modified: src/main.rs

--- rust_tutorials/generic_a/src/main.rs
+++ rust_tutorials/generic_a/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
-}
+/** 
+ * Removing Duplication by Extracting a Function 
+ * 
+ */ 
+
+ // Example - Code to find the largest number in two lists of numbers 
+
+ fn main() {
+    let number_list = vec![34, 50, 68, 55, 100]; 
+
+    let mut largest = &number_list[0]; 
+
+    for number in &number_list { 
+        if number > largest { 
+            largest = number;
+        }
+    } 
+
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8]; 
+
+    let mut largest = &number_list[0]; 
+
+    for number in &number_list { 
+        if number > largest { 
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+ }
