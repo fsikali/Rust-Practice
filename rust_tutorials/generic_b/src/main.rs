26698f66c342ef1f14054f05d2237c35a81e0943
fsikali <flemmingsikali499@gmail.com>
2024-10-28 19:26:52 +0300

    modified: src/main.rs
    new file: Cargo.lock

--- rust_tutorials/generic_b/src/main.rs
+++ rust_tutorials/generic_b/src/main.rs
@@ -31,7 +31,23 @@ fn main() {
     println!("The largest number is {}", result); 
     assert_eq!(*result, 100); 
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8]; 
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];  
+
+    let result = largest(&number_list);
     println!("The largest number is {}", result); 
-    assert_eq(*result, 6000);
-}
+    assert_eq!(*result, 6000);
+} 
+
+/* The largest number has a parameter called list, which represents any concrete slice
+of i32 values we might pass into the function.
+As a result, when we call the function, the code runs on the specific values that 
+we pass in.
+
+Steps used to change the code from listing
+  1.) Identify duplicate code
+  2.) Extract the duplicate code into the body of the function and specify the inputs and
+      return values of that code in the function signature
+  3.) Update the two instances of duplicated code to call the function instead 
+  
+ */ 
+
