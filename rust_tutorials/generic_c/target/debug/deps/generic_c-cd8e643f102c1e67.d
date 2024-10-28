c7a62604c90b62401607581d13a9b8bed3ceffab
fsikali <flemmingsikali499@gmail.com>
2024-10-28 22:15:28 +0300

    modified: src/main.rs

---
+++ rust_tutorials/generic_c/target/debug/deps/generic_c-cd8e643f102c1e67.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_c/target/debug/deps/generic_c-cd8e643f102c1e67: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_c/target/debug/deps/generic_c-cd8e643f102c1e67.d: src/main.rs
+
+src/main.rs:
