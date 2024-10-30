fecd11aecd0527f2fc9d881075dde3f71dde81f2
fsikali <flemmingsikali499@gmail.com>
2024-10-30 22:25:15 +0300

    initial commit

---
+++ rust_tutorials/generic_d/target/debug/deps/generic_d-2f19aac9ef34c4d9.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_d/target/debug/deps/generic_d-2f19aac9ef34c4d9: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_d/target/debug/deps/generic_d-2f19aac9ef34c4d9.d: src/main.rs
+
+src/main.rs:
---
+++ rust_tutorials/generic_d/target/debug/generic_d.d
@@ -0,0 +1,1 @@
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_d/target/debug/generic_d: /home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_d/src/main.rs
