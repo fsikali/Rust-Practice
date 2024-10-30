f6976abdd9e45476cc0d5d98078dc260a958b1cf
fsikali <flemmingsikali499@gmail.com>
2024-10-30 23:39:55 +0300

    initial commit

---
+++ rust_tutorials/generic_f/target/debug/deps/generic_f-281fa5f9577b0152.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_f/target/debug/deps/generic_f-281fa5f9577b0152: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_f/target/debug/deps/generic_f-281fa5f9577b0152.d: src/main.rs
+
+src/main.rs:
---
+++ rust_tutorials/generic_f/target/debug/generic_f.d
@@ -0,0 +1,1 @@
+/home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_f/target/debug/generic_f: /home/fsikali/Desktop/Task/Practice/Rust/rust_pack/rust_tutorials/generic_f/src/main.rs
