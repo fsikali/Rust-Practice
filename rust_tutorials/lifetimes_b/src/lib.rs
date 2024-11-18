f9f1b2a7b76cdfea1ffb12884fc7693c51f30b35
fsikali <flemmingsikali499@gmail.com>
2024-11-19 00:54:27 +0300

    modified: src/lib.rs

--- rust_tutorials/lifetimes_b/src/lib.rs
+++ rust_tutorials/lifetimes_b/src/lib.rs
@@ -5,7 +5,5 @@
    - Here, x has the lifetime 'b, which in this case is longer than 'a.
    - This means r can reference x because Rust knows that the reference in r
      will always be valid while x is valid.
-
-   -
      
 */
