8333d16da14b689414d4ba0c00ca077c8f589857
fsikali <flemmingsikali499@gmail.com>
2024-11-20 23:46:34 +0300

    Initial Commit

---
+++ rust_tutorials/lifetimes_e/target/debug/deps/lifetimes_e-5ca06bd5c0e15e87.d
@@ -0,0 +1,7 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/lifetimes_e/target/debug/deps/liblifetimes_e-5ca06bd5c0e15e87.rmeta: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/lifetimes_e/target/debug/deps/liblifetimes_e-5ca06bd5c0e15e87.rlib: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/lifetimes_e/target/debug/deps/lifetimes_e-5ca06bd5c0e15e87.d: src/lib.rs
+
+src/lib.rs:
