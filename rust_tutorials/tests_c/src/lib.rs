954221c4b29f0a41a79278871e90047f7450a090
fsikali <flemmingsikali499@gmail.com>
2024-12-04 11:48:22 +0300

    modified: src/lib.rs

--- rust_tutorials/tests_c/src/lib.rs
+++ rust_tutorials/tests_c/src/lib.rs
@@ -1,3 +1,13 @@
+/*
+    - Changed the name of the it_works function to exploration
+    - Added another test, that fails 
+    - Tests fails when something in the test function panics
+    - Each test is run in a new thread, and when the main thread sees that a test thread has died, the
+      test is marked as failed.
+    - The simplest way to panic is to call the panic! macro
+
+*/
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -18,12 +28,4 @@ mod tests {
     }
 } 
 
-/*
-    - Changed the name of the it_works function to exploration
-    - Added another test, that fails 
-    - Tests fails when something in the test function panics
-    - Each test is run in a new thread, and when the main thread sees that a test thread has died, the
-      test is marked as failed.
-    - The simplest way to panic is to call the panic! macro
 
-*/
