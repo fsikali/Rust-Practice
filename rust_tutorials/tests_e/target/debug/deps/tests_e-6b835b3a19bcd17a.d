7bc3958c20641d802253ca146804fdc24b656009
fsikali <flemmingsikali499@gmail.com>
2024-12-05 09:34:31 +0300

    Initial Commit

---
+++ rust_tutorials/tests_e/target/debug/deps/tests_e-6b835b3a19bcd17a.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/tests_e/target/debug/deps/tests_e-6b835b3a19bcd17a: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/tests_e/target/debug/deps/tests_e-6b835b3a19bcd17a.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust_tutorials/tests_e/target/debug/deps/tests_e-a886ff37e4105471.d
@@ -0,0 +1,7 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/tests_e/target/debug/deps/libtests_e-a886ff37e4105471.rmeta: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/tests_e/target/debug/deps/libtests_e-a886ff37e4105471.rlib: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/tests_e/target/debug/deps/tests_e-a886ff37e4105471.d: src/lib.rs
+
+src/lib.rs:
