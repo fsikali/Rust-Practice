7871e11288d8baaa75fd485ddec9545aec1e3891
fsikali <flemmingsikali499@gmail.com>
2025-01-22 18:02:32 +0300

    First commit

---
+++ rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6db64fdcfd0a278.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6db64fdcfd0a278: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6db64fdcfd0a278.d: src/main.rs
+
+src/main.rs:
---
+++ rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6f3c6abe9ab14f3.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6f3c6abe9ab14f3: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/project/minigrep/target/debug/deps/minigrep-b6f3c6abe9ab14f3.d: src/lib.rs
+
+src/lib.rs:
