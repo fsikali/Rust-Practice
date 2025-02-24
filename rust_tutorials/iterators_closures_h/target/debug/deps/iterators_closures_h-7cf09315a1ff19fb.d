2845ad9a720615a25ba855a96e598f448fd0c741
fsikali <flemmingsikali499@gmail.com>
2025-02-24 15:48:38 +0300

    new file: iterators_closures/h

---
+++ rust_tutorials/iterators_closures_h/target/debug/deps/iterators_closures_h-7cf09315a1ff19fb.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/iterators_closures_h/target/debug/deps/iterators_closures_h-7cf09315a1ff19fb: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/iterators_closures_h/target/debug/deps/iterators_closures_h-7cf09315a1ff19fb.d: src/main.rs
+
+src/main.rs:
---
+++ rust_tutorials/iterators_closures_h/target/debug/deps/iterators_closures_h-b948cf54a2834eb0.d
@@ -0,0 +1,7 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/iterators_closures_h/target/debug/deps/libiterators_closures_h-b948cf54a2834eb0.rmeta: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/iterators_closures_h/target/debug/deps/libiterators_closures_h-b948cf54a2834eb0.rlib: src/lib.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/iterators_closures_h/target/debug/deps/iterators_closures_h-b948cf54a2834eb0.d: src/lib.rs
+
+src/lib.rs:
