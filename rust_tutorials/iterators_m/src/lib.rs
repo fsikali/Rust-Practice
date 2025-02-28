52c800684f87983ca8cecdda5143a625d04003c9
fsikali <flemmingsikali499@gmail.com>
2025-02-28 11:46:58 +0300

    new file: iterators_m/...

---
+++ rust_tutorials/iterators_m/src/lib.rs
@@ -0,0 +1,17 @@
+/*
+--- Making Code Clearer with Iterators Adaptors
+*/
+
+// Example: The implementation of the search function 
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
