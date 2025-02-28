1c1d77e8bb1952bf86cb64eb87bbf2766faf41de
fsikali <flemmingsikali499@gmail.com>
2025-02-28 11:13:30 +0300

    new file: iterators_l/Cargo.toml
    iterators_l/src/lib.rs
    
    new file: iterators_l/src/main.rs

---
+++ rust_tutorials/iterators_l/src/lib.rs
@@ -0,0 +1,32 @@
+/*
+--- Using Iterator Trait Methods Instead of Indexing
+*/
+
+// Eaxmple: Changing the body of Config::build to use iterator methods
+
+impl Config {
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
