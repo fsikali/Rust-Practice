d6d69913c2786e1134816bae64e388e867bbe22f
fsikali <flemmingsikali499@gmail.com>
2025-03-02 15:11:18 +0300

    Initial Commit

---
+++ rust_tutorials/cargo_crates_d/target/debug/adder.d
@@ -0,0 +1,1 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/cargo_crates_d/target/debug/adder: /home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/cargo_crates_d/adder/src/main.rs
---
+++ rust_tutorials/cargo_crates_d/target/debug/deps/adder-f406d31415747e92.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/cargo_crates_d/target/debug/deps/adder-f406d31415747e92: adder/src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/cargo_crates_d/target/debug/deps/adder-f406d31415747e92.d: adder/src/main.rs
+
+adder/src/main.rs:
