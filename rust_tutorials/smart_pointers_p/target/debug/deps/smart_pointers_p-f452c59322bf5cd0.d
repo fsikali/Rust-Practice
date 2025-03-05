b1f22bee07cf53d0e23adf5514ece4eff9f700dc
fsikali <flemmingsikali499@gmail.com>
2025-03-05 11:51:43 +0300

    new file: smart_pointers_q/..

---
+++ rust_tutorials/smart_pointers_p/target/debug/deps/smart_pointers_p-f452c59322bf5cd0.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/smart_pointers_p/target/debug/deps/smart_pointers_p-f452c59322bf5cd0: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/smart_pointers_p/target/debug/deps/smart_pointers_p-f452c59322bf5cd0.d: src/main.rs
+
+src/main.rs:
