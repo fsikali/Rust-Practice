4424c5df15edd1145f8d6e927d42937c5e8fcfe9
fsikali <flemmingsikali499@gmail.com>
2025-03-18 12:59:04 +0300

    new file: concurrency_m/..

---
+++ rust_tutorials/concurrency_m/target/debug/deps/concurrency_m-5aefab6060d40a49.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/concurrency_m/target/debug/deps/concurrency_m-5aefab6060d40a49: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/concurrency_m/target/debug/deps/concurrency_m-5aefab6060d40a49.d: src/main.rs
+
+src/main.rs:
