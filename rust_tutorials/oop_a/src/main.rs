ec9ed4b13e80b20e46d976ea538ba4be6c2ed3e7
fsikali <flemmingsikali499@gmail.com>
2025-03-18 15:37:02 +0300

    new file: oop_a/

---
+++ rust_tutorials/oop_a/src/main.rs
@@ -0,0 +1,10 @@
+/*
+--- An AveragedCollection struct that maintains a list of integers and the average of the items in the
+    collection
+*/
+
+pub struct AveragedCollection { 
+    list: Vec<i32>, 
+    average: f64,
+}
+
