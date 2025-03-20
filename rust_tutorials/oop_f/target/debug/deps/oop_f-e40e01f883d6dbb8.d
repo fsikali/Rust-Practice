11664ecd6442cc21d3429d3c4d9d26449491db14
fsikali <flemmingsikali499@gmail.com>
2025-03-20 12:28:11 +0300

    new file: oop_f

---
+++ rust_tutorials/oop_f/target/debug/deps/oop_f-e40e01f883d6dbb8.d
@@ -0,0 +1,5 @@
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/oop_f/target/debug/deps/oop_f-e40e01f883d6dbb8: src/main.rs
+
+/home/fsikali/Desktop/Task/Practice/Rust/Tutorials/rust_pack/rust_tutorials/oop_f/target/debug/deps/oop_f-e40e01f883d6dbb8.d: src/main.rs
+
+src/main.rs:
