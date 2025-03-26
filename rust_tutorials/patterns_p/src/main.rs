65eca0b966bb9e313d7cdc7c13efd0b1efe43d26
fsikali <flemmingsikali499@gmail.com>
2025-03-26 19:54:20 +0300

    modified: patterns_n/..
    new file: patterns_o/..
    
    new file: patterns_p/..

---
+++ rust_tutorials/patterns_p/src/main.rs
@@ -0,0 +1,20 @@
+/*
+--- Using an underscore within patterns that match Some variants when we don't need to 
+    use the value inside the Some
+*/
+
+fn main() {
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+}
