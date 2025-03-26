116c79ce2d821f4ee7667d8e015fd287f17a00de
fsikali <flemmingsikali499@gmail.com>
2025-03-26 13:53:05 +0300

    new file: patterns_d/..

---
+++ rust_tutorials/patterns_d/src/main.rs
@@ -0,0 +1,8 @@
+// Using a pattern to destructure a tuple and create three variables at once 
+// Every time you've used a let statement like this you've been using patterns, although you might
+// not have realized it! More formally, a let statement looks like this:
+// let PATTERN = EXPRESSION;
+
+fn main() { 
+    let(x, y, z) = (1, 2, 3);
+}
